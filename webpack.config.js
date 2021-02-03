2fed650533c3a9620c2a668a748f6b1ae340e023
unk774 <nwmfmm@gmail.com>
2021-02-03 22:32:18 +0400

    non lazy load

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,16 @@
 const path = require('path');
-const webpack = require('webpack');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    entry: {
-        'my-lib': '/src/components/index.tsx',
-        'my-lib.min': '/src/components/index.tsx'
-    },
+    entry: '/src/components/index.tsx',
     output: {
-        path: path.resolve(__dirname, '_bundles'),
+        path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        libraryTarget: 'umd',
-        library: 'MyLib',
-        umdNamedDefine: true
     },
     resolve: {
         extensions: ['.js', '.ts','.jsx', '.tsx']
     },
     devtool: 'source-map',
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                uglifyOptions: {
-                    mangle: {
-                        keep_fnames: true,
-                    },
-                },
-            }),
-        ],
-    },
     module: {
         rules: [
             {
