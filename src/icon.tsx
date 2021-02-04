b6a995ac5f326ba49865761d60facf1d60a7ae2c
unk774 <nwmfmm@gmail.com>
2021-02-04 11:47:16 +0400

    webpack

--- src/icon.tsx
+++
@@ -1,1 +0,0 @@
-export { default as NeoIcon } from './components/icon';
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/components/index.tsx',
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'lib'),
         filename: '[name].js',
